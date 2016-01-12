504dac60ada96b6c89bc1bb9c5bb01224a3b20eb
MYounas <younus.ubit56@gmail.com>
2016-01-12 15:01:32 -0800

    App Start
    Setting Up Proj

---
+++ VS/uniManageSys/uniManageSys/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace uniManageSys.Models
+{
+    [Table("Admin")]
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ VS/uniManageSys/uniManageSys/Models/DbcsContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace uniManageSys.Models
+{
+    public class DbcsContext:DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
