4f9d0df08c44e0a1dad72386b01ad04bdd041637
MYounas <younus.ubit56@gmail.com>
2016-01-13 02:58:09 -0800

    Login/out Done
    Login/out Done

---
+++ VS/uniManageSys/uniManageSys/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using uniManageSys.Models;
+using WebMatrix.WebData;
+
+namespace uniManageSys.Controllers
+{
+    public class AdminController : Controller
+    {
+        DbcsContext db = new DbcsContext();
+
+        public ActionResult Login()
+        {
+            if (Session["admin"] == null)
+                return View();
+            else
+            {
+                return RedirectToAction("yes");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var data = db.Admins.Single(x => x.Id == 1);
+            if (string.Equals(data.UserName, admin.UserName) && string.Equals(data.Password, admin.Password))
+            {
+                Session["admin"] = admin.UserName;
+                return RedirectToAction("yes");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult yes()
+        {
+            if (Session["admin"] == null)
+                return RedirectToAction("Login");
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session["admin"] = null;
+            return RedirectToAction("Login");
+        }
+    }
+}
--- VS/uniManageSys/uniManageSys/Global.asax.cs
+++ VS/uniManageSys/uniManageSys/Global.asax.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebMatrix.WebData;
 
 namespace uniManageSys
 {
